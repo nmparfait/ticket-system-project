06e5bc05c940bc4b612be582d9ba77fb1680bc64
Parfait NM <mbamu.developer@gmail.com>
2022-01-21 12:16:01 -0500

    hero done

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ $(document).ready(() => {
     $('#nav-menu').toggleClass('active');
   });
 });
+
+// setting owl carousel
+
+let navText = [
+  "<i class='bx bx-chevron-left'></i>",
+  "<i class='bx bx-chevron-right'></i>",
+];
+
+$('#hero-carousel').owlCarousel({
+  items: 1,
+  dots: false,
+  loop: true,
+  nav: true,
+  navText: navText,
+  autoplay: true,
+  autoplayHoverPause: true,
+});
