75dcab408ca74428da042f7b1e19b146f4d28303
Parfait NM <mbamu.developer@gmail.com>
2022-01-25 22:33:12 -0500

    lancamento done

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ $('#hero-carousel').owlCarousel({
   autoplay: true,
   autoplayHoverPause: true,
 });
+
+$('#lancamento-slide').owlCarousel({
+  items: 2,
+  dots: false,
+  loop: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    500: {
+      items: 3,
+    },
+    1280: {
+      items: 4,
+    },
+    1600: {
+      items: 6,
+    },
+  },
+});
